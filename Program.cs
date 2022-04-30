e34d590ba11063e0a705db30c1b7a43d2cb68455
willamybraga <willamybraga@yahoo.com>
2022-04-30 19:10:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ProjetoDotNet.src.Entities;
+
+namespace dotnet__poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero arus = new Hero("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+            Knight knight = new Knight("Knight", 23, "Knight");
+            Wedge knight1 = new Wedge("Knight", 20 "Knight");
+
+            //Console.WriteLine(arus.Attack);
+            Console.WriteLine(wizard.Attack(7));
+            Console.WriteLine(wizard.Attack(1));
+        }
+    }
+}
---
+++ src/Entities/Wedge.cs
@@ -0,0 +1,12 @@
+namespace ProjetoDotNet.src.Entities
+{
+    public class Wedge : Hero
+    {
+        public Wedge(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+    }
+}
